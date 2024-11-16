560d2f3d882db41bb8ab2c941170ee2f6dc1f268
habeba101 <habebaahmed101@gmail.com>
2024-11-17 00:36:11 +0200

    Python Session 1 Task

---
+++ 1- Python Module/Session 1/Session1L1.py
@@ -0,0 +1,5 @@
+student={"name":"Habeba",
+         "age":2,
+         "major":"Computer and Control Engineering"}
+
+print("My name is \"Mohamed Bakr\", I'm graduated\nI'm Interested in\n\t programming \n\t EMBEDDED.")
---
+++ 1- Python Module/Session 1/Session1L2.py
@@ -0,0 +1,9 @@
+vowel_list=['a','e','i','o','u','A','E','I','O','U']
+char_1='h'
+char_2='e'
+char_3='A'
+char_4='I'
+print(char_1 in vowel_list)
+print(char_2 in vowel_list)
+print(char_3 in vowel_list)
+print(char_4 in vowel_list)
